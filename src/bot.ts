36cc283a8245d97cef274d04d5873c2edbcb5c87
SonicandTailsCD <80169260+SonicandTailsCD@users.noreply.github.com>
2023-04-27 14:43:20 -0700

    Bot update to allow it to join my main server :)

--- src/bot.ts
+++ src/bot.ts
@@ -8,8 +8,8 @@ const { pathfinder, Movements } = pathfinderPkg;
 // Here, we set up the server connection (in this case, my skin plugin server)
 console.log('Registering the bot and allowing node to control it...');
 export const bot = createBot({
-	host: 'SonicJavaBots.aternos.me',
-	port: 37867,
+	host: 'SonicandTailsCDplay.aternos.me',
+	port: 17444,
 	username: 'SonicandTailsCDb',
 	version: '1.18.2'
 });
