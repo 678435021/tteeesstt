658230d25dc0980f42c4b8ac43b9b022f6e7ebb6
SonicandTailsCD <80169260+SonicandTailsCD@users.noreply.github.com>
2023-05-03 15:49:41 -0700

    AAAAAAAAAAAAAAAAAAAAA

--- src/bot.ts
+++ src/bot.ts
@@ -124,9 +124,10 @@ export async function botCommandMode (daname: string) {
 			}
 		}
 		if (message === "Ignore someone...") {
-			await bot.waitForTicks(100)
 			bot.chat("Say the name of the person you'd like me to ignore. :)")
+			await bot.waitForTicks(100)
 			botIgnoreMode(daname, message)
+			return;
 		}
 		if (message === "Stop ignoring someone...") {
 			bot.chat("Say the name of the person you'd like me to stop ignoring. :)")
